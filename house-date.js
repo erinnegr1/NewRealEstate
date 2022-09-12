6bd3897cfb07df2e42304abfb3c3d6e5feadfd70
Erinne Grant <erinne.grant@gmail.com>
2022-09-12 16:45:19 -0400

    house dummy data

---
+++ house-date.js
@@ -0,0 +1,35 @@
+const houseData = [
+    {
+    address: ["508 St Cloud Rd"],
+    propertyType: ["house"],
+    city: ["Los Angeles"],
+    state: ['California']
+    },
+    {
+    address: ["5 Hanover Square"],
+    propertyType: ["apartment"],
+    city: ["New York"],
+    state: ['New York']
+    },
+    {
+    address: ["1600 Pennsylvania Avenue"],
+    propertyType: ["house"],
+    city: ["Washington"],
+    state: ['District of Columbia']
+    },
+    {
+    address: ["129 West 81st Street"],
+    propertyType: ["condo"],
+    city: ["New York"],
+    state: ['New York']
+    },
+    ];
+    
+    
+    /**
+    address: ["123 Main Street"],
+    propertyType: ["house", "apartment", "condo"],
+    city: ["Anytown"],
+    state: ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+    },
+    */
