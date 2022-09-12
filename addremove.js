2891883cf60ba466d842f73eb73ae11def91a6ba
Jenny Chen <rosette@Jennys-MacBook-Air.local>
2022-09-12 17:11:59 -0400

    fixed html, and add function

--- addremove.js
+++ addremove.js
@@ -1,27 +1,29 @@
-const addressSearch = document.querySelector(".street-address"); //address searchbar
-const propertySearch = document.querySelector(".property-type"); //property dropdown
-const citySearch = document.querySelector(".city"); //city searchbar
-const stateSearch = document.querySelector(".state"); //state dropdown
+// const addressSearch = document.querySelector(".street-address"); //address searchbar
+// const propertySearch = document.querySelector(".property-type"); //property dropdown
+// const citySearch = document.querySelector(".city"); //city searchbar
+// const stateSearch = document.querySelector(".state"); //state dropdown
 
 //Submit Button
 const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("click", function() {
-    let addressInput = addressSearch.value;
-    let propertyInput = propertySearch.value;
-    let cityInput = citySearch.value;
-    let stateInput = stateSearch.value;
-  
     const li = document.createElement("li");
-    console.log(addressInput + propertyInput + cityInput + stateInput);
-    li.textContent = addressInput + propertyInput + cityInput + stateInput;
+    const addressSearch = document.querySelector("input.street-address").value; //address searchbar
+    const propertySearch = document.querySelector("input.property-type").value; //property dropdown
+    const citySearch = document.querySelector("input.city").value; //city searchbar
+    const stateSearch = document.querySelector("input.state").value; //state dropdown
+  
+    console.log(addressSearch + propertySearch +
+        citySearch +stateSearch);
+    li.textContent = `${addressSearch}  ${propertySearch} 
+         ${citySearch}  ${stateSearch}`;
     const ul = document.querySelector("ul");
-    ul.appendChild("li");
+    ul.append(li);
 });
 
 
 //Street Address
-const street = document.querySelector(".street-address");
-street.addEventListener("input", function () {
-    return street.input.value;
-    //input information needs to be added to searchEngine in the 'address' key
-});
+// const street = document.querySelector(".street-address");
+// street.addEventListener("input", function () {
+//     return street.input.value;
+//     //input information needs to be added to searchEngine in the 'address' key
+// });
